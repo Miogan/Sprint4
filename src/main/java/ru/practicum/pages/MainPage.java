606d78f876bf28b25d208734e031fe639c39fc78
Joshka <odnaluna@bk.ru>
2025-09-02 16:06:24 +0300

    SP4

--- src/main/java/ru/practicum/pages/MainPage.java
+++ src/main/java/ru/practicum/pages/MainPage.java
@@ -17,7 +17,6 @@ public class MainPage {
     private final By locatorButtonOrderUp = By.cssSelector(".Button_Button__ra12g");
     private final By locatorButtonOrderDown = By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM");
 
-
     public MainPage(WebDriver driver) {
         this.driver = driver;
     }
