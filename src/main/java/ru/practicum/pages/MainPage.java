39bc94fdde2eaece9117fcd8014c577e26314b3f
Joshka <odnaluna@bk.ru>
2025-09-02 15:50:52 +0300

    SP4 (2 test)

--- src/main/java/ru/practicum/pages/MainPage.java
+++ src/main/java/ru/practicum/pages/MainPage.java
@@ -16,7 +16,7 @@ public class MainPage {
     private final By locatorButtonCookiesWindow = By.className("App_CookieButton__3cvqF");
     private final By locatorButtonOrderUp = By.cssSelector(".Button_Button__ra12g");
     private final By locatorButtonOrderDown = By.cssSelector(".Button_Button__ra12g.Button_Middle__1CSJM");
-    
+
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
